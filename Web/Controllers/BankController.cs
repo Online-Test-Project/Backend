1c74334bc0ce1f580ee9c020a2c8fca27e5b7398
Haupc <haupc.uet@gmail.com>
2019-10-01 18:53:47 +0700

    first run

--- Web/Controllers/BankController.cs
+++ Web/Controllers/BankController.cs
@@ -4,12 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Web.Repository;
 
 namespace Web.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class BankController : ControllerBase
     {
+        [HttpGet]
+        public bool create()
+        {
+            IBankRepository bank = new BankRepository(new Models.OnlineTestContext());
+            bank.Create(new Models.DTO.BankDTO
+            {
+                Description = "asdf",
+                id = new Guid("00000000-0000-0000-0000-000000000000"),
+                ModifiedDate = DateTime.Now,
+                Name = "zcvzcx"
+            });
+            return true;
+        }
+                
     }
 }
---
+++ Web/Repository/BankRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Models;
+using Web.Models.DTO;
+
+namespace Web.Repository
+{
+    
+    interface IBankRepository
+    {
+        Boolean Create(BankDTO bank);
+
+        Task<bool> Update(BankDTO bank);
+
+        Task<bool> Delete(Guid bankId);
+
+    }
+    public class BankRepository : IBankRepository
+    {
+        Guid emptyGuid = new Guid("00000000-0000-0000-0000-000000000000");
+        private OnlineTestContext DbContext;
+        public BankRepository(OnlineTestContext _context)
+        {
+            DbContext = _context;
+        }
+        public Boolean Create(BankDTO bank)
+        {
+            DbContext.QuestionBanks.Add(new QuestionBank
+            {
+                Id = Guid.NewGuid(),
+                Description = bank.Description,
+                ModifiedDate = bank.ModifiedDate,
+                Name = bank.Name,
+                OwnerId = emptyGuid,
+            });
+            DbContext.SaveChanges();
+            return true;
+
+        }
+
+        public Task<bool> Delete(Guid bankId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(BankDTO bank)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
