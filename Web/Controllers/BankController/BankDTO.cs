c6180c1ecb80b66521aefbac0483ffc4556f0199
thanhvh99 <vuhathanh00@gmail.com>
2019-10-03 20:49:00 +0700

    Move BankDTO to controller folder

---
+++ Web/Controllers/BankController/BankDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Models;
+
+namespace Web.Controllers.BankController
+{
+    public class BankDTO
+    {
+        public Guid Id { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+        public BankDTO(QuestionBank bank)
+        {
+            Id = bank.Id;
+            Name = bank.Name;
+            Description = bank.Description;
+            ModifiedDate = bank.ModifiedDate;
+        }
+    }
+}
