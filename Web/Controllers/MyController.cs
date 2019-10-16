78615878f9cd81e3337d2a6032501ce1dd8d1d99
thanhvh99 <vuhathanh00@gmail.com>
2019-10-16 21:22:53 +0700

    Update MyController.cs

--- Web/Controllers/MyController.cs
+++ Web/Controllers/MyController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Web.Models;
 
 namespace Web.Controllers
 {
@@ -11,6 +12,13 @@ namespace Web.Controllers
     [ApiController]
     public class MyController : ControllerBase
     {
+
+        public OnlineTestContext DbContext;
         public Guid userId;
+
+        public MyController(OnlineTestContext _DbContext)
+        {
+            DbContext = _DbContext;
+        }
     }
 }
